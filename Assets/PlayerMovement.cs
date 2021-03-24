1fe8b4c8eda6148445f1120500fa65cc6f8db881
myersca2024 <myers.ca@northeastern.edu>
2021-03-24 13:39:23 -0500

    Updated for multiplayer movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool isPlayerOne;
     public float speed = 5;
     public Rigidbody2D rb;
 
@@ -11,8 +12,49 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        if (isPlayerOne)
+        {
+            movement.x = 0;
+            movement.y = 0;
+            if (Input.GetKey(KeyCode.W))
+            {
+                movement.y = 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                movement.x = -1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                movement.y = -1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                movement.x = 1;
+            }
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            movement.x = 0;
+            movement.y = 0;
+            if (Input.GetKey(KeyCode.I))
+            {
+                movement.y = 1;
+            }
+            if (Input.GetKey(KeyCode.J))
+            {
+                movement.x = -1;
+            }
+            if (Input.GetKey(KeyCode.K))
+            {
+                movement.y = -1;
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                movement.x = 1;
+            }
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
     }
 }
