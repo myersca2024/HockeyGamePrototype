174bb224b014d45f695b39a2eeb8afced5dfa666
myersca2024 <myers.ca@northeastern.edu>
2021-03-24 16:13:33 -0500

    2 Player Functionality complete

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,54 +7,65 @@ public class PlayerMovement : MonoBehaviour
     public bool isPlayerOne;
     public float speed = 5;
     public Rigidbody2D rb;
+    public bool canMove = true;
+    public bool isActivePlayer;
 
     Vector2 movement;
 
     void FixedUpdate()
     {
-        if (isPlayerOne)
+        if (canMove)
         {
-            movement.x = 0;
-            movement.y = 0;
-            if (Input.GetKey(KeyCode.W))
+            if (isPlayerOne)
             {
-                movement.y = 1;
+                movement.x = 0;
+                movement.y = 0;
+                if (Input.GetKey(KeyCode.W))
+                {
+                    movement.y = 1;
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    movement.x = -1;
+                }
+                if (!isActivePlayer)
+                {
+                    if (Input.GetKey(KeyCode.S))
+                    {
+                        movement.y = -1;
+                    }
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    movement.x = 1;
+                }
+                rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
             }
-            if (Input.GetKey(KeyCode.A))
+            else
             {
-                movement.x = -1;
+                movement.x = 0;
+                movement.y = 0;
+                if (Input.GetKey(KeyCode.I))
+                {
+                    movement.y = 1;
+                }
+                if (Input.GetKey(KeyCode.J))
+                {
+                    movement.x = -1;
+                }
+                if (!isActivePlayer)
+                {
+                    if (Input.GetKey(KeyCode.K))
+                    {
+                        movement.y = -1;
+                    }
+                }
+                if (Input.GetKey(KeyCode.L))
+                {
+                    movement.x = 1;
+                }
+                rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
             }
-            if (Input.GetKey(KeyCode.S))
-            {
-                movement.y = -1;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                movement.x = 1;
-            }
-            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-        }
-        else
-        {
-            movement.x = 0;
-            movement.y = 0;
-            if (Input.GetKey(KeyCode.I))
-            {
-                movement.y = 1;
-            }
-            if (Input.GetKey(KeyCode.J))
-            {
-                movement.x = -1;
-            }
-            if (Input.GetKey(KeyCode.K))
-            {
-                movement.y = -1;
-            }
-            if (Input.GetKey(KeyCode.L))
-            {
-                movement.x = 1;
-            }
-            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
         }
     }
 }
